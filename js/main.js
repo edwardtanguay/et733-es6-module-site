6e4ce7ea5bafc8775862210d0b840d9ab283baa8
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-18 10:26:22 +0200

    nnn test works

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+const sentencize = (text) => {
+	return text.charAt(0).toUpperCase() + text.slice(1) + '.';
+}
+
 const btnInfoElem = document.querySelector('.btnInfo');
-btnInfoElem.style.display = 'none';
+
+btnInfoElem.addEventListener('click', () => {
+	const messageElem = document.querySelector('.message');
+	messageElem.innerText = 'nnn';
+});
